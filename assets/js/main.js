b74194b19415878e92904af59a7e4dff6767f400
Sanjeev <sanjeevkav52@gmail.com>
2023-03-09 22:41:24 +0530

    update-contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 /*=============== SHOW SIDEBAR ===============*/
 
 
@@ -32,14 +33,45 @@ tabs.forEach(tab=>{
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container',{
+    selectors:{
+        target:'.work__card'
+    },
+    animation:{
+        duration:300
+    }
+});
 
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work__item')
+function activeWork(){
+    linkWork.forEach(L=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
 
+linkWork.forEach(L=>l.addEventListener('click',activeWork))
 
 /*===== Work Popup =====*/
 
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
 
+        portfolioItemDetails(e.target.parentElement);
+
+    }
+})
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+    
+    
+}
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+}
+// document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup())
 /*=============== SERVICES MODAL ===============*/
 
 
