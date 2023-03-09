30b4d51fe7ae434c23cbe970c9eaecb80f60f000
Sanjeev <sanjeevkav52@gmail.com>
2023-03-09 23:50:36 +0530

    update 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,33 +45,35 @@ let mixerPortfolio = mixitup('.work__container',{
 
 /*===== Link Active Work =====*/
 const linkWork = document.querySelectorAll('.work__item')
-function activeWork(){
-    linkWork.forEach(L=>l.classList.remove('active-work'))
+
+function activeWork() {
+    linkWork.forEach(l=> l.classList.remove('active-work'))
     this.classList.add('active-work')
 }
 
-linkWork.forEach(L=>l.addEventListener('click',activeWork))
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
 
 /*===== Work Popup =====*/
-
-document.addEventListener("click",(e)=>{
-    if(e.target.classList.contains("work__button")){
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")) {
         togglePortfolioPopup();
-
         portfolioItemDetails(e.target.parentElement);
-
     }
 })
-function togglePortfolioPopup(){
+
+function togglePortfolioPopup() {
     document.querySelector(".portfolio__popup").classList.toggle("open");
-    
-    
 }
 
-function portfolioItemDetails(portfolioItem){
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+
+function portfolioItemDetails(portfolioItem) {
     document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
-// document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup())
+
 /*=============== SERVICES MODAL ===============*/
 
 
@@ -79,9 +81,60 @@ function portfolioItemDetails(portfolioItem){
 
 
 /*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+// add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+  // get current scroll position
+  let scrollY = window.pageYOffset;
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute("id");
+    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+    {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+    }
+    else
+    {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+    }
+  })
+}
 
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is higher than 350 viewport height, add the show-scroll class to a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
+  window.addEventListener('scroll', scrollUp)
